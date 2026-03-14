0552fca07a3ad4977fb8a541964e1d81d103bc67
AndreFNSousa <andrefnsousa33@gmail.com>
2026-03-14 05:17:33 +0000

    Ciclico

--- CardDex.py
+++ CardDex.py
@@ -1,26 +1,31 @@
 import CardDexTools
 
-decisao = input("Criar/Eliminar uma lista (C)\nAdicionar/Remover cartas de uma lista existente (A)\nVer uma lista (V) \nEscreva algo sem ser as opçoes para sair:").strip().lower()
-if decisao.lower() == "c":
-    decc = input("Criar (C)\nEliminar (E) ").strip().lower()
-    if decc.lower() == "c":
-        CardDexTools.criar_file()
-    if decc.lower() == "e":
-        CardDexTools.elim_file()
+while True:
+    decisao = input("Criar/Eliminar uma biblioteca (C)\nAdicionar/Remover amigos de uma biblioteca existente (A)\nVer uma biblioteca (V) \nSair (S)\n").strip().lower()
+    if decisao.lower() == "c":
+        decc = input("Criar (C)\nEliminar (E) ").strip().lower()
+        if decc.lower() == "c":
+             CardDexTools.criar_file()
+        if decc.lower() == "e":
+            CardDexTools.elim_file()
 
-elif decisao.lower() == "a":
-    filetoedit = input("Qual a lista que queres editar: ").strip().lower()
-    CardDexTools.verif_file(filetoedit)
-    deca = input("Adicionar (A)\nRemover (R) ")
-    if deca.lower() == "a":
-       CardDexTools.add_nomes(filetoedit)
-    if deca.lower() == "r":
-       carta = input("Qual o nome da carta que queres eliminar: ").strip().lower()
-       CardDexTools.remover_carta(filetoedit, carta)
+    elif decisao.lower() == "a":
+        filetoedit = input("Qual o ficheiro que queres editar: ").strip().lower()
+        CardDexTools.verif_file(filetoedit)
+        deca = input("Adicionar (A)\nRemover (R) ").strip().lower()
+        if deca.lower() == "a":
+            CardDexTools.add_nomes(filetoedit)
+        if deca.lower() == "r":
+            carta = input("Qual o nome que queres eliminar: ").strip().lower()
+            CardDexTools.remover_carta(filetoedit, carta)
 
-elif decisao.lower() == "v":
-    filetosee = input("Qual o nome da lista que queres ver: ").strip().lower()
-    CardDexTools.ver_ficheiro(filetosee)
+    elif decisao.lower() == "v":
+        filetosee = input("Qual o ficheiro que queres ver: ").strip().lower()
+        CardDexTools.ver_ficheiro(filetosee)
 
-else:
-    print("Obrigado!")
+    elif decisao.lower() == "s":
+        print("Obrigado!")
+        break
+
+    else:
+        print("Comando nao valido!")
