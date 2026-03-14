3debd765e739ad64d17febcda94b63e9af046e62
AndreFNSousa <andrefnsousa33@gmail.com>
2026-03-14 05:54:27 +0000

    Extra features

--- CardDex.py
+++ CardDex.py
@@ -1,40 +1,133 @@
 import customtkinter
 import CardDexTools
 
+
 app = customtkinter.CTk()
 app.geometry("500x500")
-app.title("Gestor de Cartas")
-
-titulo = customtkinter.CTkLabel(app, text="MENU", font=("Arial", 18))
-titulo.pack(pady=5)
-
-menu = customtkinter.CTkFrame(app)
-menu.pack(pady=10, padx=10)
-
-frame_criar = customtkinter.CTkFrame(app, fg_color="transparent")
-label_c = customtkinter.CTkLabel(frame_criar, text="MENU DE CRIAÇÃO", font=("Arial", 16, "bold"))
-label_c.pack(pady=10)
-entry_nome = customtkinter.CTkEntry(frame_criar, placeholder_text="Nome da lista...")
-entry_nome.pack(pady=10)
-botao = customtkinter.CTkButton(frame_criar, text="Criar Lista", command=lambda:CardDexTools.executar_criacao(entry_nome, label_resultado))
-botao.pack(pady=10)
-label_resultado = customtkinter.CTkLabel(frame_criar, text="")
-label_resultado.pack(pady=10)
+app.title("Janelas Amarelas")
 
 
+#frame menu
+menu_frame = customtkinter.CTkFrame(app)
+menu_frame.pack(pady=10, padx=10)
+titulomenu = customtkinter.CTkLabel(menu_frame, text="Menu", font=("Arial", 18))
+titulomenu.pack(pady=5)
+btnver = customtkinter.CTkButton(menu_frame, text="Ver", fg_color="#2e4bcc", hover_color="#29badf", command=lambda:CardDexTools.mostrar_ver(menu_frame, frame_ver))
+btnver.pack(side="left", pady=10, padx=5)
+btnadc = customtkinter.CTkButton(menu_frame, text="Adicionar", fg_color="#2e4bcc", hover_color="#29badf", command=lambda:CardDexTools.mostrar_ver(menu_frame, frame_adic))
+btnadc.pack(side="top", pady=10, padx=5)
+btnrem = customtkinter.CTkButton(menu_frame, text="Remover", fg_color="#2e4bcc", hover_color="#29badf", command=lambda:CardDexTools.mostrar_ver(menu_frame, frame_remov))
+btnrem.pack(side="right", pady=10, padx=5)
+
+#frame ver
 frame_ver = customtkinter.CTkFrame(app, fg_color="transparent")
-label_v = customtkinter.CTkLabel(frame_ver, text="LISTA DE AMIGOS", font=("Arial", 16, "bold"))
-label_v.pack(pady=10)
-bibliover = customtkinter.CTkEntry(frame_ver, placeholder_text="Nome do ficheiro...")
-bibliover.pack(pady=10)
-botaover = customtkinter.CTkButton(frame_ver, text="Ver ficheiro", command=lambda:CardDexTools.ver_ficheiro(bibliover, labeltexto))
+label_ver = customtkinter.CTkLabel(frame_ver, text="Lista de Cartas", font=("Arial", 16, "bold"))
+label_ver.pack(pady=10)
+ver_nome = customtkinter.CTkEntry(frame_ver, placeholder_text="Nome da Lista...")
+ver_nome.pack(pady=10)
+botaover = customtkinter.CTkButton(frame_ver, text="Ver lista", command=lambda:CardDexTools.ver_file(ver_nome, label_resultadover))
 botaover.pack(pady=10)
-labeltexto = customtkinter.CTkLabel(frame_ver, text="")
-labeltexto.pack(pady=10)
+label_resultadover = customtkinter.CTkLabel(frame_ver, text="")
+label_resultadover.pack(pady=10)
 
-btn_c = customtkinter.CTkButton(menu, text="Criar", fg_color="#2e4bcc", hover_color="#29badf", command=lambda:CardDexTools.mostrar_criar(frame_ver, frame_criar))
-btn_c.pack(side="left", pady=10, padx=5)
-btn_v = customtkinter.CTkButton(menu, text="Ver", fg_color="#2e4bcc", hover_color="#29badf", command=lambda:CardDexTools.mostrar_ver(frame_criar, frame_ver))
-btn_v.pack(side="right", pady=10, padx=5)
+#frame adicionar
+frame_adic = customtkinter.CTkFrame(app,fg_color="transparent")
+label_adc = customtkinter.CTkLabel(frame_adic, text="Adicionar cartas", font=("Arial", 16, "bold"))
+label_adc.pack(pady=10)
+btnadcex = customtkinter.CTkButton(frame_adic, text="Lista Existente", command=lambda:CardDexTools.mostrar_ver(frame_adic, frame_adicex))
+btnadcex.pack(side="left", pady=10)
+btnadcnw = customtkinter.CTkButton(frame_adic, text="Lista Nova", command=lambda:CardDexTools.mostrar_ver(frame_adic, frame_adicnov))
+btnadcnw.pack(side="right", pady=10)
+
+#frame adicionar a existente
+frame_adicex = customtkinter.CTkFrame(app, fg_color="transparent")
+label_adcex = customtkinter.CTkLabel(frame_adicex, text="Adicionar cartas", font=("Arial", 16, "bold"))
+label_adcex.pack(pady=10)
+ver_nomee = customtkinter.CTkEntry(frame_adicex, placeholder_text="Nome da Lista...")
+ver_nomee.pack(pady=10)
+btnadclist = customtkinter.CTkButton(frame_adicex, text="Ver lista", command=lambda:CardDexTools.verif_file(ver_nomee, label_res, frame_adicex, frame_adicexname, label_adcexname))
+btnadclist.pack(pady=10)
+label_res = customtkinter.CTkLabel(frame_adicex, text="")
+label_res.pack(pady=10)
+
+#frame adicioran nomes e telemoveis a existente
+frame_adicexname = customtkinter.CTkFrame(app, fg_color="transparent")
+label_adcexname = customtkinter.CTkLabel(frame_adicexname, text=f"Adicionar cartas a ...", font=("Arial", 16, "bold"))
+label_adcexname.pack(pady=10)
+newcarta = customtkinter.CTkEntry(frame_adicexname, placeholder_text="Nome")
+newcarta.pack(side="left", pady=10)
+newtipo = customtkinter.CTkEntry(frame_adicexname, placeholder_text="Tipo")
+newtipo.pack(side="right", pady=10)
+btnadcnomeex = customtkinter.CTkButton(frame_adicexname, text="Adicionar", command=lambda:CardDexTools.add_nomesetipo(ver_nomee, newcarta, newtipo))
+btnadcnomeex.pack(pady=10)
+label_resultadadcex = customtkinter.CTkLabel(frame_adicexname, text="")
+label_resultadadcex.pack(pady=10)
+
+#frame adicionar numeros a lista nova
+frame_adicnov = customtkinter.CTkFrame(app, fg_color="transparent")
+label_adcnova = customtkinter.CTkLabel(frame_adicnov, text=f"Adicionar cartas", font=("Arial", 16, "bold"))
+label_adcnova.pack(pady=10)
+newadclis = customtkinter.CTkEntry(frame_adicnov, placeholder_text="Nome da Lista Nova...")
+newadclis.pack(pady=10)
+btnadcnova = customtkinter.CTkButton(frame_adicnov, text="Criar Lista", command=lambda:CardDexTools.criar_file(newadclis, frame_adicnov, frame_adcnometel, label_adcnometel))
+btnadcnova.pack(pady=10)
+label_resultadadcn = customtkinter.CTkLabel(frame_adicnov, text="")
+label_resultadadcn.pack(pady=10)
+
+#frame adicioran nomes e telemoveis a nova
+frame_adcnometel = customtkinter.CTkFrame(app, fg_color="transparent")
+label_adcnometel = customtkinter.CTkLabel(frame_adcnometel, text=f"Adicionar cartas a ...", font=("Arial", 16, "bold"))
+label_adcnometel.pack(pady=10)
+newcartaa = customtkinter.CTkEntry(frame_adcnometel, placeholder_text="Nome")
+newcartaa.pack(side="left", pady=10)
+newtipoo = customtkinter.CTkEntry(frame_adcnometel, placeholder_text="Tipo")
+newtipoo.pack(side="right", pady=10)
+btnadcnmtl = customtkinter.CTkButton(frame_adcnometel, text="Adicionar", command=lambda:CardDexTools.add_nomesetipo(newadclis, newcartaa, newtipoo))
+btnadcnmtl.pack(pady=10)
+label_resultadadnt = customtkinter.CTkLabel(frame_adcnometel, text="")
+label_resultadadnt.pack(pady=10)
+
+#frame remover
+frame_remov = customtkinter.CTkFrame(app, fg_color="transparent")
+label_remov = customtkinter.CTkLabel(frame_remov, text="Remover", font=("Arial", 16, "bold"))
+label_remov.pack(pady=10)
+btnremct = customtkinter.CTkButton(frame_remov, text="Cartas", command=lambda:CardDexTools.mostrar_ver(frame_remov, frame_remov_con))
+btnremct.pack(side="left", pady=10)
+btnremlis = customtkinter.CTkButton(frame_remov, text="Lista", command=lambda:CardDexTools.mostrar_ver(frame_remov, frame_remov_list))
+btnremlis.pack(side="right", pady=10)
+
+#frame remover lista
+frame_remov_list = customtkinter.CTkFrame(app, fg_color="transparent")
+label_removlist = customtkinter.CTkLabel(frame_remov_list, text=f"Remover Lista", font=("Arial", 16, "bold"))
+label_removlist.pack(pady=10)
+newremovli = customtkinter.CTkEntry(frame_remov_list, placeholder_text="Nome da Lista a remover...")
+newremovli.pack(pady=10)
+btnreml = customtkinter.CTkButton(frame_remov_list, text="Remover Lista", command=lambda:CardDexTools.elim_file(newremovli, label_resultrem))
+btnreml.pack(pady=10)
+label_resultrem = customtkinter.CTkLabel(frame_remov_list, text="")
+label_resultrem.pack(pady=10)
+
+#frame remover contactos
+frame_remov_con = customtkinter.CTkFrame(app, fg_color="transparent")
+label_removcon = customtkinter.CTkLabel(frame_remov_con, text=f"Remover cartas", font=("Arial", 16, "bold"))
+label_removcon.pack(pady=10)
+newremovcon = customtkinter.CTkEntry(frame_remov_con, placeholder_text="Nome da Lista a Remover Cartas...")
+newremovcon.pack(pady=10)
+btnremovcon = customtkinter.CTkButton(frame_remov_con, text="Selecionar Lista", command=lambda:CardDexTools.verif_file(newremovcon, label_resultremcon, frame_remov_con, frame_remov_con_li , label_removconli))
+btnremovcon.pack(pady=10)
+label_resultremcon = customtkinter.CTkLabel(frame_remov_con, text="")
+label_resultremcon.pack(pady=10)
+
+#frame remover contactos da lista
+frame_remov_con_li = customtkinter.CTkFrame(app, fg_color="transparent")
+label_removconli = customtkinter.CTkLabel(frame_remov_con_li, text="Remover cartas de...", font=("Arial", 16, "bold"))
+label_removconli.pack(pady=10)
+newremovcarta = customtkinter.CTkEntry(frame_remov_con_li, placeholder_text="Nome da carta...")
+newremovcarta.pack(pady=10)
+btnremovconli = customtkinter.CTkButton(frame_remov_con_li, text="Remover Carta", command=lambda:CardDexTools.remover_carta(newremovcon, newremovcarta))
+btnremovconli.pack(pady=10)
+label_resultremconli = customtkinter.CTkLabel(frame_remov_con_li, text="")
+label_resultremconli.pack(pady=10)
+
 
 app.mainloop()
--- CardDexTools.py
+++ CardDexTools.py
@@ -1,27 +1,27 @@
 import customtkinter
+import os
 
-def mostrar_criar(framever, framecriar):
-    framever.pack_forget()
-    framecriar.pack(pady=20, fill="both", expand=True)
+def criar_file(usercname, frameante, frameno, lbltit):
+    user = usercname.get()
+    lbltit.configure(text=f"Adicionar cartas a {user}")
+    with open(f"{user}.txt", "w", encoding="utf-8") as fileuser:
+        
+        fileuser.write("Cartas:\n")
+        print(f"A criar: {usercname}.txt")
+    frameante.pack_forget()
+    frameno.pack(pady=20, fill="both", expand=True)
 
-def mostrar_ver(frame_criar, frame_ver):
-    frame_criar.pack_forget()
-    frame_ver.pack(pady=20, fill="both", expand=True)
 
 
-def executar_criacao(filename, label_resultado):
-    nome_do_ficheiro = filename.get()
-    while True:
-        if nome_do_ficheiro.strip() == "":
-            label_resultado.configure(text="Erro: Escreve um nome!", text_color="red")
-        else:
-            with open(f"{nome_do_ficheiro}.txt", "w", encoding="utf-8") as fileuser:
-                fileuser.write("Lista de Amigos:\n")
-                print(f"A criar: {nome_do_ficheiro}.txt")
-            label_resultado.configure(text=f"Ficheiro {nome_do_ficheiro}.txt criado!", text_color="green")
-            break
+def mostrar_ver(frameant, framenova):
+    # Esconde o frame de ver e mostra o de criar
+    frameant.pack_forget()
+    framenova.pack(pady=20, fill="both", expand=True)
 
-def ver_ficheiro(filenam, labelresultado):
+
+
+def ver_file(filenam, labelresultado):
+    # 1. Pega no texto da caixa de entrada
     file = filenam.get()
     while True:
         try:
@@ -35,3 +35,64 @@ def ver_ficheiro(filenam, labelresultado):
             print(f"A criar: {file}.txt")
             labelresultado.configure(text="Ficheiro não existe!", text_color="red")
         break
+
+
+
+def add_nomesetipo(filetoedit, nome, tipo):
+    fich = filetoedit.get()
+    nnome = nome.get()
+    ttipo = tipo.get()
+    listateluser = {}
+    listateluser[nnome] = ttipo
+    with open(f"{fich}.txt", "a", encoding="utf-8") as ficheiro:
+        for nomeca, tipoca in listateluser.items():
+            ficheiro.write(f"- {nomeca}: {tipoca}\n")
+        print("Cartas adicionadas com sucesso!")
+
+
+
+def verif_file(filetoverif, labelres, framant, framnov, labeltit):
+    supfile = filetoverif.get()
+    try:
+        with open(f"{supfile}.txt", "r", encoding="utf-8") as ficheiro:
+            labeltit.configure(text=f"Adicionar cartas a {supfile}")
+            content = ficheiro.read()
+            framant.pack_forget()
+            framnov.pack(pady=20, fill="both", expand=True)
+    except FileNotFoundError:
+        labelres.configure(text="Ficheiro não existe!", text_color="red")
+
+
+def elim_file(fileee, labres):
+    userename = fileee.get()
+    if os.path.exists(f"{userename}.txt"):
+        os.remove(f"{userename}.txt")
+        print(f"O ficheiro {userename} foi eliminado!")
+        labres.configure(text=f"{userename}.txt eliminado")
+    else:
+        print("O ficheiro não foi encontrado.") 
+        labres.configure(text=f"O ficheiro {userename}.txt nao foi encontrado")
+
+
+def remover_carta(ficheironome, nomearem):
+    ficheiro_nome = ficheironome.get()
+    nomearemover = nomearem.get()
+    linhas_vivas = []
+    encontrado = False
+
+    with open(f"{ficheiro_nome}.txt", "r", encoding="utf-8") as f:
+        for linha in f:
+            # Se o nome NÃO estiver na linha, guardamos a linha
+            if nomearemover.lower() not in linha.lower():
+                linhas_vivas.append(linha)
+            else:
+                encontrado = True
+        
+     # Reescrever o ficheiro apenas com as linhas que restaram
+    with open(f"{ficheiro_nome}.txt", "w", encoding="utf-8") as f:
+        f.writelines(linhas_vivas)
+            
+    if encontrado:
+        print(f"A carta {nomearemover} foi removida.")
+    else:
+        print("Nome não encontrado.")
