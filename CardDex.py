6e73b261b3a2a7cf1f9eda14a0ce7591c0586ec7
AndreFNSousa <andrefnsousa33@gmail.com>
2026-03-14 05:10:24 +0000

    Mais Funcionalidades Extra Extra

--- CardDex.py
+++ CardDex.py
@@ -1,72 +1,26 @@
 import CardDexTools
-import os
 
-
-listac = {}
-listae = {}
-
-decisao = input("Criar/Eliminar uma lista (C)\nAdicionar/Remover cartas de uma lista existente (A)\nVer uma lista (V) \nEscreva algo sem ser as opçoes para sair:")
+decisao = input("Criar/Eliminar uma lista (C)\nAdicionar/Remover cartas de uma lista existente (A)\nVer uma lista (V) \nEscreva algo sem ser as opçoes para sair:").strip().lower()
 if decisao.lower() == "c":
-   decc = input("Criar (C)\nEliminar (E) ").strip().lower()
-   if decc.lower() == "c":
-      usercname = input("Qual o nome da lista que queres criar: ").strip().lower()
-      if os.path.exists(f"{usercname}.txt"):
-         print("O ficheiro já existe!")
-      else:
-         while True:
-            cartaum = input("Nome da carta:(ou escreve sair quando acabares): ").strip().lower()
-            if cartaum == "sair":
-               break
-            else:
-               tipoum = input(f"Tipo de {cartaum.capitalize()}: ").strip().lower()
-               listac[cartaum] = tipoum
-         with open(f"{usercname}.txt", "w", encoding="utf-8") as fileuser:
-            fileuser.write(f"Lista {usercname.capitalize()}:\n")
-            for nome, tipo in listac.items():
-               fileuser.write(f"- {nome}: {tipo}\n")
-            print("Lista criada com sucesso!")
-
-   elif decc.lower() == "e":
-      userename = input("Qual o nome do ficheiro que queres eliminar: ").strip().lower()
-      if os.path.exists(f"{userename}.txt"):
-         os.remove(f"{userename}.txt")
-         print(f"O ficheiro {userename} foi eliminado!")
-      else:
-         print("O ficheiro não foi encontrado.")
+    decc = input("Criar (C)\nEliminar (E) ").strip().lower()
+    if decc.lower() == "c":
+        CardDexTools.criar_file()
+    if decc.lower() == "e":
+        CardDexTools.elim_file()
 
 elif decisao.lower() == "a":
-   filetoedit = input("Qual a lista que queres editar: ").strip().lower()
-   try:
-      with open(f"{filetoedit}.txt", "r", encoding="utf-8") as fileuser:
-         pass
-      deca = input("Adicionar (A)\nRemover (R) ").strip().lower()
-      if deca.lower() == "a":
-         while True:
-            cartadois = input("Nome da carta:(ou escreve sair quando acabares): ").strip().lower()
-            if cartadois == "sair":
-               break
-            else:
-               tipodois = input(f"Tipo de {cartadois.capitalize()}: ").strip().lower()
-               listae[cartadois] = tipodois
-         with open(f"{filetoedit}.txt", "a", encoding="utf-8") as fileuser:
-            for nome, tipo in listae.items():
-               fileuser.write(f"- {nome}: {tipo}\n")
-            print("Nomes adicionados com sucesso!")
-      if deca.lower() == "r":
-         cartael = input("Qual o nome da carta que queres eliminar: ").strip().lower()
-         CardDexTools.remover_carta(filetoedit, cartael)
-   except FileNotFoundError:
-      print("O ficheiro ainda não existe.")
+    filetoedit = input("Qual a lista que queres editar: ").strip().lower()
+    CardDexTools.verif_file(filetoedit)
+    deca = input("Adicionar (A)\nRemover (R) ")
+    if deca.lower() == "a":
+       CardDexTools.add_nomes(filetoedit)
+    if deca.lower() == "r":
+       carta = input("Qual o nome da carta que queres eliminar: ").strip().lower()
+       CardDexTools.remover_carta(filetoedit, carta)
 
 elif decisao.lower() == "v":
-   filetosee = input("Qual o nome da lista que queres ver: ").strip().lower()
-   try:
-      with open(f"{filetosee}.txt", "r", encoding="utf-8") as fileuser:
-         conteudo = fileuser.read()
-         print("--- Conteúdo do Ficheiro ---")
-         print(conteudo)
-   except FileNotFoundError:
-      print("O ficheiro ainda não existe.")
+    filetosee = input("Qual o nome da lista que queres ver: ").strip().lower()
+    CardDexTools.ver_ficheiro(filetosee)
 
 else:
-   print("Obrigado!")
+    print("Obrigado!")
--- CardDexTools.py
+++ CardDexTools.py
@@ -1,19 +1,79 @@
-def remover_carta(ficheiro_nome, nome_a_remover):
+import os
+
+def remover_carta(ficheiro_nome, carta_a_remover):
     linhas_vivas = []
     encontrado = False
 
     with open(f"{ficheiro_nome}.txt", "r", encoding="utf-8") as f:
         for linha in f:
-            if nome_a_remover not in linha:
+            if carta_a_remover not in linha:
                 linhas_vivas.append(linha)
             else:
                 encontrado = True
         
-     # Reescrever o ficheiro apenas com as linhas que restaram
     with open(f"{ficheiro_nome}.txt", "w", encoding="utf-8") as f:
         f.writelines(linhas_vivas)
             
     if encontrado:
-        print(f"A carta {nome_a_remover.capitalize()} foi removido.")
+        print(f"A carta {carta_a_remover.capitalize()} foi removida.")
     else:
         print("Carta não encontrada.")
+
+
+def ver_ficheiro(filetosee):
+    try:
+       with open(f"{filetosee}.txt", "r", encoding="utf-8") as file:
+        conteudo = file.read()
+        print("--- Conteúdo do Ficheiro ---")
+        print(conteudo)
+    except FileNotFoundError:
+        print("A lista ainda não existe.")
+
+
+def verif_file(filetoverif):
+    try:
+        with open(f"{filetoverif}.txt", "r", encoding="utf-8") as ficheiro:
+           content = ficheiro.read()
+    except FileNotFoundError:
+        print("A lista ainda não existe.")
+
+
+def add_nomes(filetoedit):
+    listateluser = {}
+    while True:
+        carta = input("Nome da carta(ou escreve sair quando acabares): ").strip().lower()
+        if carta == "sair":
+            break
+        else:
+            ctipo= input(f"Tipo de {carta.capitalize()}: ").strip().lower()
+            listateluser[carta] = ctipo
+    with open(f"{filetoedit}.txt", "a", encoding="utf-8") as ficheiro:
+        for nome, tipo in listateluser.items():
+            ficheiro.write(f"- {nome}: {tipo}\n")
+        print("Carta/s adicionada/s com sucesso!")
+
+
+def elim_file():
+    userename = input("Qual o nome da lista que queres eliminar: ").strip().lower()
+    if os.path.exists(f"{userename}.txt"):
+        os.remove(f"{userename}.txt")
+        print(f"A lista {userename} foi eliminada!")
+    else:
+        print("A lista não foi encontrado.")  
+
+
+def criar_file():
+    listatel = {}
+    usercname = input("Qual o nome do ficheiro que queres criar: ").strip().lower()
+    while True:
+        nomec = input("Nome da carta(ou escreve sair quando acabares): ").strip().lower()
+        if nomec == "sair":
+            break
+        else:
+            tipoc = input(f"Tipo de {nomec.capitalize()}: ").strip().lower()
+            listatel[nomec] = tipoc
+    with open(f"{usercname}.txt", "w", encoding="utf-8") as fileuser:
+        fileuser.write("Lista de Amigos:\n")
+        for nome, tipo in listatel.items():
+            fileuser.write(f"- {nome}: {tipo}\n")
+        print("Lista criada com sucesso!")  
